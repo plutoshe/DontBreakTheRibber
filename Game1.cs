d5226f828588edbaab37ab1e66a4da9e9247075a
Dane Carter <u0645467@ad.utah.edu>
2018-08-23 12:01:27 -0600

    first commmit

---
+++ Game1.cs
@@ -0,0 +1,207 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Windows.Graphics.Display;
+using System;
+using Windows.UI.ViewManagement;
+
+namespace DontBreakTheRubber
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+
+        const float SKYRATIO = 2f / 3f;
+
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+
+        SpriteClass spikeBall;
+
+        bool spaceDown;
+        bool gameStarted;
+        bool gameOver;
+
+        float screenWidth;
+        float screenHeight;
+
+        float ballBounceSpeed;
+        float gravitySpeed;
+
+
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            graphics.IsFullScreen = false;
+            graphics.PreferredBackBufferWidth = 600;
+            graphics.PreferredBackBufferHeight = 600;
+
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+
+            screenHeight = ScaleToHighDPI((float)ApplicationView.GetForCurrentView().VisibleBounds.Height);
+            screenWidth = ScaleToHighDPI((float)ApplicationView.GetForCurrentView().VisibleBounds.Width);
+
+            spaceDown = false;
+            gameStarted = false;
+
+            ballBounceSpeed = ScaleToHighDPI(-1200f);
+            gravitySpeed = ScaleToHighDPI(30f);
+            this.IsMouseVisible = false;
+
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            
+
+            spikeBall = new SpriteClass(GraphicsDevice, "Content/characterSprite.png", ScaleToHighDPI(1f));
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            float elapsedTime = (float)gameTime.ElapsedGameTime.TotalSeconds; // Get time elapsed since last Update iteration
+
+
+            KeyboardHandler();
+
+            if (gameOver)
+            {
+                spikeBall.dX = 0;
+                spikeBall.dY = 0;
+            }
+
+            spikeBall.Update(elapsedTime);
+
+            spikeBall.dY += gravitySpeed;
+
+            if (spikeBall.y > screenHeight * SKYRATIO)
+            {
+                spikeBall.dY = 0;
+                spikeBall.y = screenHeight * SKYRATIO;
+            }
+
+            //if (spikeBall.y <= screenHeight * SKYRATIO)
+            //{
+            //    spaceDown = true;
+            //}
+
+            spikeBall.dA = 7f;
+
+            base.Update(gameTime);
+        }
+
+
+        protected override void Draw(GameTime gameTime)
+        {
+
+            GraphicsDevice.Clear(Color.CornflowerBlue); // Clear the screen
+
+            spriteBatch.Begin();
+
+            spikeBall.Draw(spriteBatch);
+
+            spriteBatch.End();
+
+
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+
+        public float ScaleToHighDPI(float f)
+        {
+            DisplayInformation d = DisplayInformation.GetForCurrentView();
+            f *= (float)d.RawPixelsPerViewPixel;
+            return f;
+        }
+
+
+        public void StartGame()
+        {
+            // Reset dino position
+            spikeBall.x = screenWidth / 2;
+            spikeBall.y = screenHeight * SKYRATIO;
+            gameStarted = true;
+        }
+
+        void KeyboardHandler()
+        {
+            KeyboardState state = Keyboard.GetState();
+
+            // Quit the game if Escape is pressed.
+            if (state.IsKeyDown(Keys.Escape)) Exit();
+
+            // Start the game if Space is pressed.
+            // Exit the keyboard handler method early, preventing the dino from jumping on the same keypress.
+            if (!gameStarted)
+            {
+                if (state.IsKeyDown(Keys.Space))
+                {
+                    StartGame();
+                    gameStarted = true;
+                    spaceDown = true;
+                    gameOver = false;
+                }
+                return;
+            }
+
+            // Restart the game if Enter is pressed
+            if (gameOver)
+            {
+                if (state.IsKeyDown(Keys.Enter))
+                {
+                    StartGame();
+                    gameOver = false;
+                }
+            }
+
+            // Jump if Space (or another jump key) is pressed
+            if (state.IsKeyDown(Keys.Space) || state.IsKeyDown(Keys.W) || state.IsKeyDown(Keys.Up))
+            {
+                // Jump if Space is pressed but not held and the dino is on the floor
+                if (!spaceDown && spikeBall.y >= screenHeight * SKYRATIO - 1) spikeBall.dY = ballBounceSpeed;
+
+                spaceDown = true;
+            }
+            else spaceDown = false;
+        }
+    }
+}
---
+++ SpriteClass.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DontBreakTheRubber
+{
+    class SpriteClass
+    {
+        const float HITBOXSCALE = .5f; // experiment with this value to make the collision detection more or less forgiving
+
+        // sprite texture
+        public Texture2D texture
+        {
+            get;
+        }
+
+        // x coordinate of the center of the sprite
+        public float x
+        {
+            get;
+            set;
+        }
+
+        // y coordinate of the center of the sprite
+        public float y
+        {
+            get;
+            set;
+        }
+
+        // Angle of the sprite around central axis
+        public float angle
+        {
+            get;
+            set;
+        }
+
+        // Rate of change of x per second
+        public float dX
+        {
+            get;
+            set;
+        }
+
+        // Rate of change of y per second
+        public float dY
+        {
+            get;
+            set;
+        }
+
+        // Rate of change of angle per second
+        public float dA
+        {
+            get;
+            set;
+        }
+
+        // Scale of the texture, where 1 is its true size
+        public float scale
+        {
+            get;
+            set;
+        }
+
+        // Constructor
+        public SpriteClass(GraphicsDevice graphicsDevice, string textureName, float scale)
+        {
+            this.scale = scale;
+
+            // Load the specified texture
+            var stream = TitleContainer.OpenStream(textureName);
+            texture = Texture2D.FromStream(graphicsDevice, stream);
+        }
+
+        // Update the position and angle of the sprite based on each rate of change and the time elapsed
+        public void Update(float elapsedTime)
+        {
+            this.x += this.dX * elapsedTime;
+            this.y += this.dY * elapsedTime;
+            this.angle += this.dA * elapsedTime;
+        }
+
+        // Draw the sprite with the given SpriteBatch
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            // Determine the position vector of the sprite
+            Vector2 spritePosition = new Vector2(this.x, this.y);
+
+            // Draw the sprite
+            spriteBatch.Draw(texture, spritePosition, null, Color.White, this.angle, new Vector2(texture.Width / 2, texture.Height / 2), new Vector2(scale, scale), SpriteEffects.None, 0f);
+        }
+
+        // Detect collision between two rectangular sprites
+        public bool RectangleCollision(SpriteClass otherSprite)
+        {
+            if (this.x + this.texture.Width * this.scale * HITBOXSCALE / 2 < otherSprite.x - otherSprite.texture.Width * otherSprite.scale / 2) return false;
+            if (this.y + this.texture.Height * this.scale * HITBOXSCALE / 2 < otherSprite.y - otherSprite.texture.Height * otherSprite.scale / 2) return false;
+            if (this.x - this.texture.Width * this.scale * HITBOXSCALE / 2 > otherSprite.x + otherSprite.texture.Width * otherSprite.scale / 2) return false;
+            if (this.y - this.texture.Height * this.scale * HITBOXSCALE / 2 > otherSprite.y + otherSprite.texture.Height * otherSprite.scale / 2) return false;
+            return true;
+        }
+
+    }
+}
