cffef2fdbad03109efcff010d388117671760582
Dane Carter <u0645467@ad.utah.edu>
2018-08-28 11:04:29 -0600

    fixed the previous fix

--- Game1.cs
+++ Game1.cs
@@ -314,7 +314,7 @@ namespace DontBreakTheRubber
         {
             if(upToDown)
             {
-                if ((angle >= 330 && angle <= 360) || (angle >= 0 && angle <= 30))
+                if ((angle >= 315 && angle <= 360) || (angle >= 0 && angle <= 45))
                 {
                     score += 3;
                     ballBounceSpeed += -400;
